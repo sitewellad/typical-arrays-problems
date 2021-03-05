b1406beb3a7e451336e7002caff61ceadbe43d38
sitewellad <sitewellad@gmail.com>
2021-03-05 13:53:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+exports.min = function min(array) {
+    return !Array.isArray(array) || !array.length
+        ? 0
+        : array.sort((a, b) => a - b)[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return !Array.isArray(array) || !array.length
+        ? 0
+        : array.sort((a, b) => a - b)[array.length - 1];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let sum = 0;
+    if (Array.isArray(array) && array.length > 0) {
+        array.forEach((item) => (sum += item));
+        return sum / array.length;
+    } else return 0;
+};
